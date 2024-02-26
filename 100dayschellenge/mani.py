85639db7ea88ac816a58a7d1c35b11be7f386381
Mani Kumar Gupta <mg9263645@gmail.com>
2024-02-26 15:43:07 +0530

    1st commit

---
+++ 100dayschellenge/mani.py
@@ -0,0 +1,41 @@
+'''print("hello")
+a = str(input("enter the number 1 :- ",))
+b = str(input("enter the number 2 :- ",))
+c = str(input("enter the number 3 :- ",))
+d = str(input(j"enter the number 4 :- ",))
+e = str(input("enter the number 5 :- ",))
+print(a + b + c + d + e)
+#module is the code which writen alredy adu cycle improment not invention. it install by :- pip install "name of module". there are some bultine modules.
+#first programe date 20 feb 2024
+print("hello word")
+#please dont use file names of modules.
+#pythone as a calculater.
+print(5+6+7+8, 99/22)
+#comments are the lines which ignore by interpiter.
+#for multiline commentuse:-3times'
+print("mani the seeker")
+print("the dohol is vaging")
+#if i want to remove the space or dono ko same line me lane keliye at the end of print statmentes add "end = """"
+print("mani the seeker", end = "")
+print("the dohol is vaging")
+#for any specific caracter add anything between comma here its 4.
+print("mani the seeker", end = "4")
+print("the dohol is vaging")`
+print("mani the seeker", "the dohol is vaging", end = "4")
+#escape sequance characters any like \n for new line
+print("mani the seeker \n the dohol is vaging")
+#variable is like a continer ye sirf valu stor karta jisko age kisi bhe time use karsakte hai. just like we define x for anything. variable are of 3 type float ex 34.89  int eg 1 string eg anything but no mathematical expresion applicable.
+ 
+a = input("enter the number:- ", )
+b = input("do you want to enter new number ",)
+print(int(a) + int(b))'''
+#by default inpute is a string
+a = input("enter the number:- ", )
+b = input("do you want to enter new number ",)
+# for integer use int() for float () for str()
+#how mulltiplication work on string it's just write it x times
+print(10 * (a+b))
+#for real multiplication on inpute make inpute as int
+print(10 * int((a+b)))
+print(10 * int((int(a)+int(b))))
+pr
